3d2595020338e99ed1b4ec96f725ec4d79bd0522
Manuel Maute <manuel.maute@bradbit.com>
2020-09-14 10:14:09 +0200

    Add optional flags for lazygit, lazydocker and docker-compose

--- init.sh
+++ init.sh
@@ -1,3 +1,35 @@
+# Flag to determine if docker-compose should be installed
+INSTALL_COMPOSE=0
+# Flag to determine if lazygit and lazydocker
+INSTALL_LAZY=0
+
+# Print arguments help
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 --flagA --flagB ... | --help"
+   echo "\t--with-compose Include Docker-Compose"
+   echo "\t--with-lazy Include lazygit and lazydocker"
+   echo "\t--help Show usage help"
+   exit 1 # Exit script after printing help
+}
+
+# idiomatic parameter and option handling
+while test $# -gt 0
+do
+  case "$1" in
+    --with-compose) INSTALL_COMPOSE=1
+        ;;
+    --with-lazy) INSTALL_LAZY=1
+        ;;
+    --help) helpFunction
+        ;;
+    --*) echo "bad option $1"
+        ;;
+  esac
+  shift
+done
+
 # add repositories
 # NOTE: yes is actually a command and streams a "y"
 # to EVERY promt of the piped command. If you want to
@@ -12,9 +44,8 @@ echo 'libssl1.1 libraries/restart-without-asking boolean true' | debconf-set-sel
 # install necessary packages in noninteractive mode
 yes | DEBIAN_FRONTEND=noninteractive apt-get update && apt install vim git curl iputils-ping open-iscsi lazygit zsh -y 
 
-
 echo "\n********************************\n********************************\n"
-echo ">>> ALL PACKAGES INSTALLED"
+echo ">>> ALL DEFAULT PACKAGES INSTALLED"
 echo "\n********************************\n********************************\n"
 
 
@@ -39,22 +70,47 @@ echo ">>> ZSH INITIALIZED"
 echo "\n********************************\n********************************\n"
 
 
-# install lazydocker
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh > lzd_install.sh
-sed -i 's/sudo//g' lzd_install.sh
-bash lzd_install.sh
-rm lzd_install.sh
+installLazy() {
+  # install lazygit
+  yes | lazygit
+  # install lazydocker
+  curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh > lzd_install.sh
+  sed -i 's/sudo//g' lzd_install.sh
+  bash lzd_install.sh
+  rm lzd_install.sh
+  echo "\n********************************\n********************************\n"
+  echo ">>> LAZYGIT AND LAZYDOCKER INSTALLED"
+  echo "\n********************************\n********************************\n"
+}
 
-# install rancher
+if [ $INSTALL_COMPOSE -eq 1 ]
+then installLazy
+fi
+
+# install docker with rancher install script
 yes | curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
-# install compose
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# apply permissions
-chmod +x /usr/local/bin/docker-compose
+echo "\n********************************\n********************************\n"
+echo ">>> DOCKER INSTALLED"
+echo "\n********************************\n********************************\n"
 
-echo "\n********************************\n********************************\n"
-echo ">>> DOCKER(-COMPOSE) INSTALLED"
-echo "\n********************************\n********************************\n"
+
+installCompose() {
+  # install compose
+  curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  # apply permissions
+  chmod +x /usr/local/bin/docker-compose
+
+  echo "\n********************************\n********************************\n"
+  echo ">>> DOCKER-COMPOSE INSTALLED"
+  echo "\n********************************\n********************************\n"
+}
+
+
+if [ $INSTALL_COMPOSE -eq 1 ]
+then installCompose
+fi
+
 
 echo "***** Script Done! *****"
+exit 1
